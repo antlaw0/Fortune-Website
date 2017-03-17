f22405b4cd6b2ab04d2058218aa09f66a7d14113
AAA <aaa@aaa.com>
2017-03-17 16:33:33 -0500

    fixed meadowlark.js issue with code from class repo

--- meadowlark.js
+++ meadowlark.js
@@ -1,351 +1,53 @@
-var 
-express 
-= 
-require 
-( 
-'express' 
-); 
-var 
-app 
-= 
-express 
-(); 
-app 
-. 
-set 
-( 
-'port' 
-, 
-process 
-. 
-env 
-. 
-PORT 
-|| 
-3000 
-); 
-// custom 404 page 
-app 
-. 
-use 
-( 
-function 
-( 
-req 
-, 
-res 
-){ 
-res 
-. 
-type 
-( 
-'text/plain' 
-); 
-app
-.
-get
-(
-'/'
-,
-function
-(
-req
-,
-res
-){
-res
-.
-type
-(
-'text/plain'
-);
-res
-.
-send
-(
-'Meadowlark Travel'
-);
-});
-app
-.
-get
-(
-'/about'
-,
-function
-(
-req
-,
-res
-){
-res
-.
-type
-(
-'text/plain'
-);
-res
-.
-send
-(
-'About Meadowlark Travel'
-);
-});
-// custom 404 page
-app
-.
-use
-(
-function
-(
-req
-,
-res
-,
-next
-){
-res
-.
-type
-(
-'text/plain'
-);
-res
-.
-status
-(
-404
-);
-res
-.
-send
-(
-'404 - Not Found'
-);
-});
-res 
-. 
-status 
-( 
-404 
-); 
-res 
-. 
-send 
-( 
+var express = require('express');
 
-'404 -Not Found' 
-); }); 
-// custom 500 page 
-app 
-. 
-use 
-( 
-function 
-( 
-err 
-, 
-req 
-, 
-res 
-, 
-next 
-){ 
-console 
-. 
-error 
-( 
-err 
-. 
-stack 
-); 
-res 
-. 
-type 
-( 
-'text/plain' 
-); 
-res 
-. 
-status 
-( 
-500 
-); 
-res 
-. 
-send 
-( 
-'500 -Server Error' 
-); }); 
-app 
-. 
-listen 
-( 
-app 
-. 
-get 
-( 
-'port' 
-), 
-function 
-(){ 
-console 
-.
-log
-(
-'Express started on http://localhost:'
-+
-app
-.
-get
-(
-'port'
-)
-+
-'; press Ctrl-C to terminate.'
-);
+var app = express();
+
+// Set up template
+var handlebars = require('express-handlebars').create({defaultLayout:'main'});
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+
+// Set the location for static files, images, client javascripts, stylesheets...
+app.use(express.static(__dirname + '/public'))
+
+var fortunes=[
+  "Conquer your fears or they will conquer you.",
+  "Rivers need springs.",
+  "Do not fear what you don't know.",
+  "You will have a pleasant surprise.",
+  "Whenever possible, keep it simple.",
+];
+
+// Route handlers for pages
+
+app.get('/', function(req, res){
+  res.render('home');
 });
 
-//adding handlebars
-var
-app
-=
-express
-();
-// set up handlebars view engine
-var
-handlebars
-=
-require
-(
-'express-handlebars'
-)
-.
-create
-({
-defaultLayout
-:
-'main'
-});
-app
-.
-engine
-(
-'handlebars'
-,
-handlebars
-.
-engine
-);
-app
-.
-set
-(
-'view engine'
-,
-'handlebars'
-);
 
-//views
-app
-.
-get
-(
-'/'
-,
-function
-(
-req
-,
-res
-)
-{
-res
-.
-render
-(
-'home'
-);
+app.get('/about', function(req, res){
+  var randomFortune = fortunes[Math.floor(Math.random() * fortunes.length)];
+  res.render('about', {fortune: randomFortune})
 });
-app
-.
-get
-(
-'/about'
-,
-function
-(
-req
-,
-res
-)
-{
-res
-.
-render
-(
-'about'
-);
+
+
+//404 page
+app.use(function(req, res){
+  res.status(404);
+  res.render('404');
 });
-// 404 catch-all handler (middleware)
-app
-.
-use
-(
-function
-(
-req
-,
-res
-,
-next
-){
-res
-.
-status
-(
-404
-);
-res
-.
-render
-(
-'404'
-);
+
+// 500 Error page
+app.use(function(err, req, res, next){
+  console.error(err.stack);
+  res.status(500);
+  res.render('500');
 });
-// 500 error handler (middleware)
-app
-.
-use
-(
-function
-(
-err
-,
-req
-,
-res
-,
-next
-){
-console
-.
-error
-(
-err
-.
-stack
-);
-res
-.
-status
-(
-500
-);
-res
-.
-render
-(
-'500'
-);
+
+// Set up the port that the server will listen on, and start server
+
+app.set('port', process.env.PORT || 3000);
+
+app.listen(app.get('port'), function(){
+  console.log('Server is running on http://localhost/' + app.get('port'));
 });
